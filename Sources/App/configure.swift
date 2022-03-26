6bed280ca7d3e143554d38f96d10f4000fe31766
Noah Pikielny <np299@cornell.edu>
2022-03-26 17:09:12 -0400

    Postgress Configuration

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -7,6 +7,7 @@ public func configure(_ app: Application) throws {
     // uncomment to serve files from /Public folder
     // app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
 
+    #if DEBUG
     app.databases.use(.postgres(
         hostname: Environment.get("DATABASE_HOST") ?? "localhost",
         port: Environment.get("DATABASE_PORT").flatMap(Int.init(_:)) ?? PostgresConfiguration.ianaPortNumber,
@@ -14,6 +15,15 @@ public func configure(_ app: Application) throws {
         password: Environment.get("DATABASE_PASSWORD") ?? "vapor_password",
         database: Environment.get("DATABASE_NAME") ?? "vapor_database"
     ), as: .psql)
+    #else
+    guard let databaseURL = Environment.get("DATABASE_URL"), var postgresConfig = PostgresConfiguration(url: databaseURL) else { throw Abort(.internalServerError) }
+    
+    postgresConfig.tlsConfiguration = .makeClientConfiguration()
+    postgresConfig.tlsConfiguration?.certificateVerification = .none
+    app.databases.use(.postgres(
+        configuration: postgresConfig
+    ), as: .psql)
+    #endif
 
     app.migrations.add(CreatePosts())
     try app.autoMigrate().wait()
