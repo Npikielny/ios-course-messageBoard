40aef2c7ce00cf0e6ad7040dada54e4f8442b177
Noah Pikielny <np299@cornell.edu>
2022-03-26 16:24:02 -0400

    Initial Commit

---
+++ Sources/App/Controllers/PostController.swift
@@ -0,0 +1,95 @@
+//
+//  PostController.swift
+//  
+//
+//  Created by Noah Pikielny on 3/19/22.
+//
+
+import Fluent
+import Vapor
+
+struct PostController: RouteCollection {
+    // Adds routes
+    func boot(routes: RoutesBuilder) throws {
+        let posts = routes.grouped("posts")
+        posts.get("all", use: getAllPosts)
+        posts.get(":postID", use: getPost)
+        
+        posts.post(use: create)
+        
+        posts.put(":postID", use: update)
+        
+        posts.delete(":postID", use: delete)
+        posts.delete("reset", ":username", ":password", use: reset)
+    }
+    
+    func getAllPosts(req: Request) -> EventLoopFuture<[Post]> {
+        if let poster = req.parameters.get("poster") {
+            return Post.query(on: req.db)
+                .filter(\.$poster == poster)
+                .all()
+        }
+        return Post.query(on: req.db).all()
+    }
+    
+    func getPost(req: Request) throws -> EventLoopFuture<Post> {
+        Post.find(req.parameters.get("postID"), on: req.db)
+            .unwrap(or: RouteError.notFound)
+    }
+    
+    func create(req: Request) throws -> EventLoopFuture<Post> {
+        guard let post = try? req.content.decode(Post.self) else { throw RouteError.unsupportedMediaType }
+        if let _ = post.poster, let _ = post.body, let _ = post.title {
+            return post.save(on: req.db).transform(to: post)
+        } else {
+            throw RouteError.partialInformation("Requires poster, body, and title")
+        }
+    }
+    
+    func update(req: Request) throws -> EventLoopFuture<Post> {
+        guard let newPost = try? req.content.decode(Post.self) else { throw RouteError.unsupportedMediaType }
+        
+        guard let id: UUID = req.parameters.get("postID"),
+              let poster = newPost.poster else {
+                  throw RouteError.partialInformation("Requires poster: <String> body")
+              }
+        
+        return Post.query(on: req.db)
+            .filter(\.$id == id)
+            .filter(\.$poster == poster)
+            .first()
+            .unwrap(or: RouteError.notFound)
+            .flatMap { post in
+                post.body = newPost.body;
+                return post.update(on: req.db)
+                    .transform(to: post)
+            }
+    }
+    
+    func reset(req: Request) throws -> EventLoopFuture<HTTPStatus> {
+        if req.parameters.get("username") == "iOS Instructors" && req.parameters.get("password") == "I Love Walker White" { // change these to secrets
+            return Post.query(on: req.db).all()
+                .map { $0.delete(on: req.db) }
+                .transform(to: .ok)
+        } else {
+            throw RouteError.unauthorized
+        }
+    }
+    
+    func delete(req: Request) throws -> EventLoopFuture<Post> {
+        guard let postInformation = try? req.content.decode(Post.self) else { throw RouteError.unsupportedMediaType }
+        guard let id: UUID = req.parameters.get("postID"),
+              let poster = postInformation.poster else {
+                  throw RouteError.partialInformation("Requires poster: <String>")
+              }
+        
+        return Post.query(on: req.db)
+            .filter(\.$id == id)
+            .filter(\.$poster == poster)
+            .first()
+            .unwrap(or: RouteError.notFound)
+            .flatMap { post in
+                post.delete(on: req.db).transform(to: post)
+            }
+    }
+}
---
+++ Sources/App/Migrations/CreatePosts.swift
@@ -0,0 +1,25 @@
+//
+//  CreatePost.swift
+//  
+//
+//  Created by Noah Pikielny on 3/19/22.
+//
+
+import FluentKit
+
+// Manages Posts table changes
+struct CreatePosts: AsyncMigration {
+    func prepare(on database: Database) async throws {
+        try await database.schema(Post.schema)
+            .id()
+            .field("title", .string, .required)
+            .field("body", .string, .required)
+            .field("poster", .string, .required)
+            .field("timeStamp", .datetime, .required)
+            .create()
+    }
+    
+    func revert(on database: Database) async throws {
+        try await database.schema(Post.schema).delete()
+    }
+}
